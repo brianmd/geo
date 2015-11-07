ce12b0399e703ab84656a436e6b8b4a8970fd7ac
Brian Murphy-Dye <brian@murphydye.com>
2015-11-06 21:57:27 -0700

    Add GeoLocation.

---
+++ lib/geo_location.rb
@@ -0,0 +1,31 @@
+module Geo
+  module_function
+
+  class GeoLocation
+    include Virtus.model
+
+    attribute :latitude, Float
+    attribute :longitude, Float
+
+    def distance_from(other_location)
+      #GeoDistance.distance(latitude, longitude, other_location.latitude, other_location.longitude)
+      dist = GeoDistance::Haversine.geo_distance(latitude, longitude, other_location.latitude, other_location.longitude)
+      dist.as_miles
+    end
+  end
+
+  def GeoLocation(location)
+    case location
+    when GeoLocation
+      return location
+    when String
+      tokens = location.split(',')
+      lat  = Float(tokens[0])
+      long = Float(tokens[1])
+      return GeoLocation.new latitude: lat, longitude: long
+    else
+      fail 'Unable to coerce into GeoLocation'
+    end
+  end
+end
+
---
+++ spec/geo_location_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+require 'geo_location'
+
+module Geo
+  describe GeoLocation do
+    let(:verve) { Geo::GeoLocation('33.1243208, -117.32582479999996') }
+
+    context 'when instantiated from a string' do
+      it 'should be an instance of GeoLocation' do
+        expect(verve.class).to eq(GeoLocation)
+      end
+
+      it 'should have acceptable precision' do
+        expect(verve.latitude).to be_within(0.0000001).of(33.1243208)
+      end
+
+      it 'should calculate the correct distance' do
+        home = Geo::GeoLocation('35.149005,-106.5770737')
+        miles = verve.distance_from(home)
+        expect(miles).to be_within(0.000000001).of(630.2960908921099)
+      end
+    end
+  end
+end
+
